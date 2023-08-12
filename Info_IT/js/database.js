ad0625bd3ac2143cd83374c99c54e982c611e9d0
A.S.BOSE <98217889+Bose14@users.noreply.github.com>
2023-08-12 16:01:23 +0530

    changes by bose

--- Info_IT/js/database.js
+++ Info_IT/js/database.js
@@ -1,20 +1,23 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCuAe-oYofR6xq7NbO7MPdFiXZv8lC88lI",
-    authDomain: "ietcertify.firebaseapp.com",
-    databaseURL: "https://ietcertify-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "ietcertify",
-    storageBucket: "ietcertify.appspot.com",
-    messagingSenderId: "633521793502",
-    appId: "1:633521793502:web:723c70e3c32ccbe4b3554f",
-    measurementId: "G-304YJXJNLN"
-  };
+    apiKey: "AIzaSyCiGr_X_0bB7-238gd25TyIHheiCPWLw88",
+  authDomain: "accelerp-2c3ce.firebaseapp.com",
+  databaseURL: "https://accelerp-2c3ce-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "accelerp-2c3ce",
+  storageBucket: "accelerp-2c3ce.appspot.com",
+  messagingSenderId: "829162797306",
+  appId: "1:829162797306:web:336caf5e5e70d8c7e87143",
+  measurementId: "G-4R2XWGTR2B"
+};
   
+
   // Initialize Firebase
   const app = firebase.initializeApp(firebaseConfig);
   const auth = firebase.auth(); 
   const database = firebase.database(); 
+  var database_ref = database.ref(); 
+  const storage = firebase.storage();
 
-
+//STUDENT LOGIN
   function stulogin(){
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
@@ -22,16 +25,87 @@ const firebaseConfig = {
     auth.signInWithEmailAndPassword(username, password).then(()=>{
         var user=auth.currentUser;
         var uid=user.uid;
-        console.log(uid)
-        window.location.href="/Info_IT/html/student/student-info/student.html"
+
+        firebase.database().ref('user/'+uid).once('value').then(function(snapshot){
+            var name=snapshot.val().name;
+            var role=snapshot.val().role;
+            //var mobile=snapshot.val().mobile;
+
+            if(role=="student"){
+                window.location.href="/Info_IT/html/faculty/faculty-info/faculty.html"
+            }
+            else{
+                alert("You are not a student");
+            }
+        })
+    
+
+
+       
     })
     .catch((error)=>{
         alert("Login failed!")
     })
 
 }
+//ADMIN LOGIN
+function adlogin(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
 
+    auth.signInWithEmailAndPassword(username, password).then(()=>{
+        var user=auth.currentUser;
+        var uid=user.uid;
 
+        firebase.database().ref('user/'+uid).once('value').then(function(snapshot){
+            var name=snapshot.val().name;
+            var role=snapshot.val().role;
+            //var mobile=snapshot.val().mobile;
+
+            if(role=="admin"){
+                window.location.href="/Info_IT/html/admin/admin-info/admin-info.html"
+            }
+            else{
+                alert("You are not a faculty");
+            }
+        })
+    
+    .catch((error)=>{
+        alert("Login failed!")
+    })
+
+})
+}
+//FACULTY LOGIN
+function faclogin(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    auth.signInWithEmailAndPassword(username, password).then(()=>{
+        var user=auth.currentUser;
+        var uid=user.uid;
+
+        firebase.database().ref('user/'+uid).once('value').then(function(snapshot){
+            var name=snapshot.val().name;
+            var role=snapshot.val().role;
+            //var mobile=snapshot.val().mobile;
+
+            if(role=="faculty"){
+                window.location.href="/Info_IT/html/student/student-info/student.html"
+            }
+            else{
+                alert("You are not a faculty");
+            }
+        })
+    
+    .catch((error)=>{
+        alert("Login failed!")
+    })
+
+})
+}
+
+//INTERN SUBMIT
 
 function submitintern(){
     firebase.auth().onAuthStateChanged((user) => {
@@ -43,7 +117,95 @@ function submitintern(){
 
     var event = document.getElementById("exampleEvent").value;
     var org = document.getElementById("exampleOrg").value;
+    var duration = document.getElementById("exampleDuration").value;
+    var date1 = document.getElementById("exampleDate").value;
+    // console.log(date)
+    let startdate=  date1.replace(/-/g, "");
+    var enddate = document.getElementById("exampleDate2").value;
+    var domain = document.getElementById("exampleDomain").value;
+    var mode = document.getElementById("exampleMode").value;
+    var certificate = document.getElementById("exampleFormControlFile1")
+    var report = document.getElementById("exampleFormControlFile2").value;
+
+
+    if(event=="" || org=="" || date1=="" || domain=="" || duration=="" || enddate=="" || mode=="")
+    {
+        document.getElementById("error").innerHTML="Fill all the details"
+        document.getElementById("error").style.color="red"
+
+    }
+    else{
+        const file = certificate.files[0];
+
+        // Validate file type and size...
+        // (same validation code as before)
+
+        // Firebase Storage configuration
+        const storage = firebase.storage();
+        const storageRef = storage.ref();
+        const filename = "images/" + file.name;
+        const progressBar = document.getElementById("progress-bar")
+
+        // Upload the file to Firebase Storage
+        const uploadTask = storageRef.child(filename).put(file);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                // You can track the upload progress here if needed
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                progressBar.style.width = `${progress}%`;
+            },
+            (error) => {
+                console.error("Error uploading file:", error);
+                alert("Error uploading file.");
+            },
+            async () => {
+                // File uploaded successfully
+
+                // Get the download URL of the uploaded image
+                const downloadURL = await uploadTask.snapshot.ref.getDownloadURL();
+
+                // Save the download URL to Firebase Realtime Database
+                const newUserRef = database.ref("stucertify/"+uid+"/"+event+"/"+startdate+"/")
+                newUserRef.set({
+                  event: event,
+                  organization: org,
+                  duration:duration,
+                  startdate: startdate,
+                  enddate:enddate,
+                  domain: domain,
+                  mode:mode,
+                  certificate:downloadURL,
+                  report:report
+                
+                })
+                alert("File uploaded successfully and URL saved to database!");
+            }
+        );
+        }
+
+}
+    })
+}
+//WORKSHOP    
+
+function submitworkshop(){
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+      
+            // User is signed in
+            console.log("User Signed In");
+            var uid = user.uid;
+
+    var event = document.getElementById("exampleEvent").value;
+    var org = document.getElementById("exampleOrg").value;
+    var topic = document.getElementById("exampleTopic").value;
+    var duration = document.getElementById("exampleDuration").value;
     var date = document.getElementById("exampleDate").value;
+    console.log(date)
+    let date1=  date.replace(/-/g, "");
+    var date2 = document.getElementById("exampleDate2").value;
     var domain = document.getElementById("exampleDomain").value;
 
     if(event=="" || org=="" || date=="" || domain==""){
@@ -51,9 +213,8 @@ function submitintern(){
         document.getElementById("error").style.color="red"
 
     }
-
     else{
-        const newUserRef = database.ref("students/"+uid).push();
+        const newUserRef = database.ref("stucertify/"+uid+"/"+event+"/"+date1+"/").push();
         newUserRef.set({
           event: event,
           organization: org,
@@ -62,9 +223,6 @@ function submitintern(){
         
         })
     }
-
     }
-})
-
-    }
-    
+})   
+}
